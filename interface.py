d3ffbe5fb54bd9658223cc530306456c2d665af4
firefistkunal <kunal28sinha@gmail.com>
2020-01-20 12:51:41 +0530

    added some comments to make it more understandable

--- interface.py
+++ interface.py
@@ -1,7 +1,11 @@
-import momoslibrary as momo
+import momoslibrary as momo # import the file that contains our functions, individual functions can also be imported
 
+# Code for the program to take neccessary  inputs from the user
 number = input("Enter an year\n")
-if momo.OddEven(number):
+
+# The relevant function is then called passing the input
+if momo.OddEven(number): # the function "OddEven()"" is written as "momo.OddEven()" because its from "momoslibrary" which is imported as "momo"
     print("Its leap year\n")
 else:
     print("This aint no leap year.\n")
+#Based on the function response the neccessary output is given to the user
--- momoslibrary.py
+++ momoslibrary.py
@@ -1,10 +1,9 @@
-def LeapYearCheck(year):
-    year=int(year)
+def LeapYearCheck(year):                          # function take the value and puts it in the variable "year"
+    year=int(year)                                # year is formatted as integer datatype
     if year%4==0 and year%100!=0 or year%400==0:
-        return True
+        return True                               # funtions returns true or 1 if the year is a leap year
 
-def OddEven(number):
-    number=int(number)
+def OddEven(number):                              # function takes the value and stores it in the variable "number"
+    number=int(number)                            # number is formatted as integer datatype
     if number%2==0:
-        return True
-    
+        return True                               # funtion returns true or 1 if the number is an even number
