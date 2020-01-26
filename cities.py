266a93bdce0d5ef4562f54509047bf084c0d0f1b
Madhura-19 <madhura1396@gmail.com>
2020-01-26 22:13:56 +0530

    matrix print prg

---
+++ cities.py
@@ -0,0 +1,22 @@
+m = int(input('number of rows, m = '))
+n = int(input('number of columns, n = '))
+mat=[]
+for i in range(0,m):
+    mat.append([])
+print(mat)
+for i in range(0,m):
+    for j in range(0,n):
+        mat[i].append(j)
+        mat[i][j]=0
+print(mat)
+for i in range(0,m):
+    for j in range(0,n):                                               
+        print ('entry in row: ',i+1,' column: ',j+1)
+        mat[i][j]=int(input())
+print (mat)
+
+   
+       
+
+
+
