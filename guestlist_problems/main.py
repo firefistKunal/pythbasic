fe38c7f8f1298e8b8f3f93d272269644630f253c
firefistkunal <kunal28sinha@gmail.com>
2020-02-11 19:04:28 +0530

    complete guestlist problem

--- guestlist_problems/main.py
+++ guestlist_problems/main.py
@@ -24,15 +24,35 @@ def csvListTranspose(filename):
         
 
         return names
+def guestDisplay(filename):
+    with open('%s.csv' % filename, newline='') as file:
+        reader=csv.reader(file)
+        print(list(reader))
+    return 0
+
 
 def strToInt(str_list): #function to conver list datatype from string to integers, pass list with strings, as argument and returns same list with integers
         int_list = list(int(x) for x in str_list)
         return int_list
 
+choice=1
+while choice:
+    choice=int(input("1.Enter guests\n2.Display guest List\n3.Average age of guest\n0.Exit\n"))
+    if choice==1:
+        guestInput()
+    elif choice==2:
+        guestDisplay('guestlist')
+    elif choice==3:
 
 
-transpose_data=csvListTranspose('guestlist')
 
-age=strToInt(transpose_data[1])
+        transpose_data=csvListTranspose('guestlist')
 
-print("Average age of guests is {} " .format(sum(age)/len(age)))
+        age=strToInt(transpose_data[1])
+
+        print("Average age of guests is {} " .format(sum(age)/len(age)))
+
+
+
+
+
