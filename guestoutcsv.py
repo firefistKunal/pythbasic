e19d908c335ae9d4af47b4f5c7f117020ac1ffab
Madhura Anand <madhura1396@gmail.com>
2020-02-11 17:34:40 +0530

    Add files via upload

---
+++ guestoutcsv.py
@@ -0,0 +1,30 @@
+import csv
+sum=0
+count=0
+
+with open(r"C:\Users\madhura.anand\Documents\guestsss.csv",'r') as f:
+    data = csv.reader(f)
+    for row in data:
+        #print(row)
+        count += 1
+        for i in row:
+            #print(i)
+            if(i.isalpha()==True):
+                print(i)
+            if(len(i)>2 and i.isnumeric()):
+                print(i)
+
+            if(len(i)==2 and i.isnumeric()):
+                sum=sum+int(i)
+
+                       
+# print(sum)
+# print(count)
+avg=sum/count
+print("avg age is",avg)
+
+        
+
+# with open("out.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerows(final1)
