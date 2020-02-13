43fb80c0d2d9e76f4e4ecc521c52be2f5e098bde
Madhura Anand <madhura1396@gmail.com>
2020-02-13 12:52:49 +0530

    Update npf.py

--- npf.py
+++ npf.py
@@ -2,9 +2,6 @@ import numpy as np
 import random 
 list1=np.asarray(random.sample(range(1, 1000), 10))
 print(list1)
-sortedarray=np.argsort(list1)
-print(sortedarray)
-# s=slice(-1,-6,-1)
-print(np.argsort(list1)[-1:-6],list1[np.argsort(list1)[-1:-6]])  
+print(np.argsort(list1)[-1:-6:-1],list1[np.argsort(list1)[-1:-6:-1]])  
 
 
