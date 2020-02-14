131d2704d4eb338cda600700a4278b91b95c5235
firefistkunal <kunal28sinha@gmail.com>
2020-02-14 11:56:18 +0530

    Correlation b/w IR and ER

---
+++ Correlation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from scipy.stats.stats import pearsonr
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('StudentIntelligence.csv')
+print(pearsonr(data['IR'], data['ER']))
+
+#To Visualize
+plt.xlabel("Intelleigence Ratio")
+plt.ylabel("Engineering ratio")
+plt.scatter(data['IR'], data['ER'])
+plt.show()
