44b29b41138d6ee95a728c0ff77d8b7fe807f696
firefistkunal <kunal28sinha@gmail.com>
2020-02-19 21:39:42 +0530

    Fixed the csv column index error

--- pan.py
+++ pan.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
-data = np.array(['a','b','c','d'])
-s = pd.Series(data)
-print(s)
+# data = np.array(['a','b','c','d'])
+# s = pd.Series(data)
+# print(s)
 
 import pandas as pd
 
-data=pd.read_csv(r'C:\Users\madhura.anand\Documents\abc.csv','rt')
+data=pd.read_csv('abc.csv')
+
+# print(data.columns)
 
 print(data['X'])
 print(data['Y'])
