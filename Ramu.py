81573a30a01255518ef081b257086f9f19195d79
firefistkunal <kunal28sinha@gmail.com>
2020-02-20 23:05:41 +0530

    exercise 3

--- Ramu.py
+++ Ramu.py
@@ -14,5 +14,15 @@ def exercise2():
         print("Not quite!!")
     return 0
 
-exercise2()
+def exercise3():
+
+    numbers = list(int(x) for x in input("Enter 3 numbers separated by space to find lowest and highest among them: ").split())
+    numbers.sort()
+    print(numbers)
+    print("\n highest number is {}\nlowest number is {}" .format(numbers[-1],numbers[0]))
+
+    return 0
+
+
+exercise3()
 
