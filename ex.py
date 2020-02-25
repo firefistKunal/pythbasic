b46dd851d8a42c887978bccbcbdbe3b8b5ff1cee
Madhura Anand <madhura1396@gmail.com>
2020-02-25 12:37:15 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+# data1 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+# arr1 = np.array(data1)
+# print(arr1)
+# print(arr1.ndim)
+# print(arr1.shape)
+# print(np.zeros(10))
+# array=([ 0, 1, 2, 3, 4, 12, 12, 12, 8, 9])
+a = np.zeros((2, 3, 4))
+print(a)
+#why is it printing 4 columns?HOW TF DOES 3D ARRAY WORK
+#O/P
+# [[[0. 0. 0. 0.]
+#   [0. 0. 0. 0.]
+#   [0. 0. 0. 0.]]
+
+#  [[0. 0. 0. 0.]
+#   [0. 0. 0. 0.]
+#   [0. 0. 0. 0.]]]
