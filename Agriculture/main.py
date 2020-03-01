83eb6b26c0839602db4c42c5984c280aa81feac4
firefistkunal <kunal28sinha@gmail.com>
2020-03-01 19:27:57 +0530

    area under cultivation for all crops in chhattisgarh till year 2014

--- Agriculture/main.py
+++ Agriculture/main.py
@@ -3,12 +3,18 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df=pd.read_csv('apy.csv')
+original=pd.read_csv('apy.csv')
 
-Cgdata=df[df['State_Name']=='Chhattisgarh']
-graph=Cgdata.groupby(['Crop_Year','Crop'])['Area'].agg(np.sum).reset_index().pivot(index='Crop', columns='Crop_Year')
-# print(graph.drop('Rice').columns)
-graph.drop('Rice').sort_values(by=('Area', 2014)).plot.barh()
+cg_data=original[original['State_Name']=='Chhattisgarh']
+data=cg_data.groupby(['Crop_Year','Crop'])['Area'].agg(np.sum).reset_index().pivot(index='Crop', columns='Crop_Year')
+# print(data.drop('Rice').columns)
+# data.drop('Rice').sort_values(by=('Area', 2014)).plot.barh()
+# plt.show()
+plt.bar(data.loc['Rice',:].reset_index()['Crop_Year'], data.loc['Rice',:].reset_index()['Rice'])
+plt.show()
+print(data.index)
+crop=input("\nWhat crop you want?")
+data.loc[crop,:].plot(kind='line', subplots=True) 
 plt.show()
 
 
